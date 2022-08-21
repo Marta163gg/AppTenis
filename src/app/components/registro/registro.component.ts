c6d8feee9902dce2bdd96eab6782768a107a59b3
Marta163gg <mggalvan@minsait.com>
2022-08-21 22:00:37 +0200

    registro add files

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import {Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'registro',
-  templateUrl: './registro.component.html'
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
 })
 
 export class RegistroComponent {
 
   public title: String;
+  public user: User;
 
   constructor(private _route: ActivatedRoute, private _router: Router){
-    this.title = 'Registro';
+    this.title = 'Pagina de registro de nuevo usuario';
+    this.user = new User('','','','','','ROLE_USER','');
   }
 
+  onSubmit(){
+    console.log(this.user);
+  }
 }
+
+
